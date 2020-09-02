ee8f00a9cfd51e4150cc0561634fe0e478b5332d
Outsider <55410852+Nodha@users.noreply.github.com>
2020-09-02 10:17:58 +0200

    Add files via upload

---
+++ jsOpdracht001/index.js
@@ -0,0 +1,13 @@
+
+console.log("Hello Winc Academy")
+let name = ('nodha');
+console.log(name);
+let som = (4+4);
+console.log(som);
+let draad = ('4'+'4');
+console.log(draad);
+let a = (5);
+console.log(a + 5);
+console.log(a / 2);
+const age = (33);
+console.log('type')
