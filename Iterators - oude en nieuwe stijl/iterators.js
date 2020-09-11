47352609304fb3a2730cbc1418a108df5d387517
Outsider <55410852+Nodha@users.noreply.github.com>
2020-09-11 09:25:55 +0200

    Add files via upload

---
+++ Iterators - oude en nieuwe stijl/iterators.js
@@ -0,0 +1,17 @@
+
+
+let colorArray = ["yellow", "green", "red", "pink"];
+colorArray.forEach((color) => console.log(color));
+
+let colors = {
+  colorOne: "blue",
+  colorTwo: "orange",
+  colorThree: "black",
+  colorFour: "white",
+  colorFive: "purple",
+};
+
+for (y in colors) {
+  console.log(colors[y]);
+}
+
